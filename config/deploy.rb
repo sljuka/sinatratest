5cbf0172b00f9b83365241b24641ce14715434ad
David Sljukic <davidsljukic@gmail.com>
2015-05-09 21:20:57 +0200

    deploy test

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,9 @@ set :deploy_to, '/home/deploy/apps/sinatratest'
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.2.2'
 
+set :passenger_restart_with_sudo, true # default
+default_run_options[:pty] = true
+
 # Default value for :scm is :git
 # set :scm, :git
 
