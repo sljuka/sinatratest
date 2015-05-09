259ec0afdb7abda003dbeddef6ad40d50c810a2e
David Sljukic <davidsljukic@gmail.com>
2015-05-09 20:50:55 +0200

    deploy test

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,11 +40,11 @@ set :rbenv_ruby, '2.2.2'
 namespace :deploy do
 
   task :start do
-    run "touch #{current_path}/tmp/restart.txt"
+    execute :touch "#{current_path}/tmp/restart.txt"
   end
 
   task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
+    execute :touch "#{current_path}/tmp/restart.txt"
   end
 
 end
