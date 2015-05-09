c8677e9e62c79e7d960d5aa0fb8e870bf31e7bac
David Sljukic <davidsljukic@gmail.com>
2015-05-09 20:39:02 +0200

    deploy test

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,10 +41,7 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      run "touch #{current_release}/tmp/restart.txt"
     end
   end
 
