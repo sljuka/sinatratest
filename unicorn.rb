2ced76ec17a3aa821bb11f2e6b73907794da95b6
David Sljukic <davidsljukic@gmail.com>
2015-05-10 15:57:39 +0200

    deo test

--- unicorn.rb
+++ unicorn.rb
@@ -5,17 +5,17 @@ working_directory "/home/deploy/apps/sinatratest/current"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/home/deploy/apps/sinatratest/current/pids/unicorn.pid"
+pid "/home/deploy/apps/sinatratest/shared/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/logs/unicorn.log"
 # stdout_path "/path/to/logs/unicorn.log"
-stderr_path "/home/deploy/apps/sinatratest/current/logs/unicorn.log"
-stdout_path "/home/deploy/apps/sinatratest/current/logs/unicorn.log"
+stderr_path "/home/deploy/apps/sinatratest/shared/logs/unicorn.log"
+stdout_path "/home/deploy/apps/sinatratest/shared/logs/unicorn.log"
 
 # Unicorn socket
 # listen "/tmp/unicorn.[app name].sock"
-listen "/home/deploy/apps/sinatratest/current/tmp/unicorn.sinatratest.sock"
+listen "/home/deploy/apps/sinatratest/shared/tmp/unicorn.sinatratest.sock"
 
 # Number of processes
 # worker_processes 4
