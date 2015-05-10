759876839b02054ebf16ce037c39da77860bfbe6
David Sljukic <davidsljukic@gmail.com>
2015-05-10 15:42:34 +0200

    dep test

--- unicorn.rb
+++ unicorn.rb
@@ -1,5 +1,6 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
+
 working_directory "/home/deploy/apps/sinatratest/current"
 
 # Unicorn PID file location
@@ -14,7 +15,7 @@ stdout_path "/home/deploy/apps/sinatratest/current/logs/unicorn.log"
 
 # Unicorn socket
 # listen "/tmp/unicorn.[app name].sock"
-listen "/home/deploy/apps/tmp/unicorn.sinatratest.sock"
+listen "/home/deploy/apps/sinatratest/current/tmp/unicorn.sinatratest.sock"
 
 # Number of processes
 # worker_processes 4
